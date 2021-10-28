85f58c411d2079c9019a707d91aecb7a1f6c6866
Mayank Mathur <mayank.mathur.mayank@outlook.com>
2021-10-28 13:23:44 +0530

    Statements and blocks are segregated and saved into JSON object

--- Scan-1/scan1.py
+++ Scan-1/scan1.py
@@ -1,4 +1,5 @@
 import os
+from typing import Tuple, Union
 
 
 class StatementBlockScanner:
@@ -10,6 +11,8 @@ class StatementBlockScanner:
         self.bufferPos = 0
         self.code = []
         self.EOF = False
+        self.refinedCode = ""
+        self.statementsNBlocks = []
         pass
 
     # get file word-by-word
@@ -35,7 +38,7 @@ class StatementBlockScanner:
         self.bufferPos += 1
         return word
 
-    # flush line buffer (to be used if single line comment founf)
+    # flush line buffer (to be used if single line comment found)
 
     def flushBuffer(self):
         self.bufferPos = 0
@@ -54,7 +57,7 @@ class StatementBlockScanner:
         self.bufferPos -= 1
         self.wordBuffer[self.bufferPos] = word[commentEndPos+2:]
 
-    # scan the file and store the code
+    # scan the file, remove comments and store the code
 
     def scan(self):
         word = self.fileReader()
@@ -73,22 +76,79 @@ class StatementBlockScanner:
             self.code.append(word)
             word = self.fileReader()
 
+        self.refinedCode = (" ").join(self.code)
         pass
+
+    def breakToBlocks(self, code):
+
+        # check if given var is minimum but not eq to -1
+        # also ignores other vars if they are -1
+        def checkMinNotEqNeg1(varComp, var1, var2) -> bool:
+            if varComp == -1:
+                return False
+            if var1 != -1 and var2 != -1:
+                return varComp < var1 and varComp < var2
+            if var1 != -1:
+                return varComp < var1
+            if var2 != -1:
+                return varComp < var2
+
+            return True  # here varComp is not -1 but other are
+
+        linesNBlocks = []
+        while len(code) > 0:
+            semiColon = code.find(";")
+            blockOpen = code.find("{")
+            blockClos = code.find("}")
+
+            if checkMinNotEqNeg1(semiColon, blockOpen, blockClos):
+                line = str(code[0:semiColon+1]).strip()
+                linesNBlocks.append({
+                    "line": line,
+                    "block": []
+                })  # include semicolon
+                code = code[semiColon+1:]
+            elif checkMinNotEqNeg1(blockOpen, semiColon, blockClos):
+                line = str(code[0:blockOpen]).strip()
+                linesNBlocks.append({
+                    "line": line,
+                    "block": []
+                })  # do not include open bracket
+                code = code[blockOpen+1:]
+                (linesNBlocks[len(linesNBlocks)-1]["block"],
+                 code) = self.breakToBlocks(code)
+            elif checkMinNotEqNeg1(blockClos, semiColon, blockOpen):
+                line = str(code[0:blockClos]).strip()
+                if len(line) > 0:
+                    linesNBlocks.append({
+                        "line": line,
+                        "block": []
+                    })  # include semicolon
+                code = code[blockClos+1:]
+                return [linesNBlocks, code]
+            else:
+                # EOF
+                return [linesNBlocks, code]
+        return [linesNBlocks, code]
+
+    def classify(self):
+        [self.statementsNBlocks, retCode] = self.breakToBlocks(
+            self.refinedCode)
 
     # print into test-output file
 
     def print(self):
         self.output.seek(0)
         self.output.truncate()
-        for word in self.code:
-            self.output.write(word + " ")
+        self.output.write(str(self.statementsNBlocks).replace("\"", "\\\""))
 
 
 def main():
     testFile = os.path.dirname(os.path.realpath(__file__)) + "\\test1.java"
-    outputFile = os.path.dirname(os.path.realpath(__file__)) + "\\output.java"
+    outputFile = os.path.dirname(os.path.realpath(__file__)) + "\\output.json"
     scanner = StatementBlockScanner(testFile, outputFile)
     scanner.scan()
+    scanner.classify()
     scanner.print()
 
 
